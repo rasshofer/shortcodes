73e98637d358da1fd7c5e46996b03765ddc90b31
Thomas Rasshofer <hello@thomasrasshofer.com>
2020-01-25 17:06:13 +0100

    Add Prettier

--- shortcodes.js
+++ shortcodes.js
@@ -4,28 +4,32 @@ const add = (generic, callback) => {
   if (typeof generic === 'string' && callback) {
     shortcodes[generic] = callback;
   } else {
-    Object.keys(generic).forEach((name) => {
+    Object.keys(generic).forEach(name => {
       shortcodes[name] = generic[name];
     });
   }
 };
 
-const parse = (input) => input.replace(/(?=[^\]])\(([a-z0-9_-]+):(.*?)\)/gmi, (match, name, values) => {
-  if (!shortcodes[name]) {
-    return match;
-  }
+const parse = input =>
+  input.replace(
+    /(?=[^\]])\(([a-z0-9_-]+):(.*?)\)/gim,
+    (match, name, values) => {
+      if (!shortcodes[name]) {
+        return match;
+      }
 
-  const attrs = {};
-  const splitted = values.split(/(\s*([a-z0-9_-]+):\s*)/gmi);
+      const attrs = {};
+      const splitted = values.split(/(\s*([a-z0-9_-]+):\s*)/gim);
 
-  attrs[name] = splitted.shift().trim();
+      attrs[name] = splitted.shift().trim();
 
-  for (let i = 0; i < splitted.length; i += 3) {
-    attrs[splitted[i + 1].trim()] = splitted[i + 2].trim();
-  }
+      for (let i = 0; i < splitted.length; i += 3) {
+        attrs[splitted[i + 1].trim()] = splitted[i + 2].trim();
+      }
 
-  return shortcodes[name](attrs);
-});
+      return shortcodes[name](attrs);
+    },
+  );
 
 module.exports = {
   add,
--- tests/test.js
+++ tests/test.js
@@ -15,19 +15,25 @@ describe('API', () => {
 // Parsing
 
 describe('Parsing', () => {
-  shortcodes.add('youtube', (attrs) => `<div class="youtube" style="padding-top:${(100 / attrs.width * attrs.height)}%"><iframe src="https://www.youtube.com/embed/${attrs.youtube}" frameborder="0"></iframe></div>`);
+  shortcodes.add(
+    'youtube',
+    attrs =>
+      `<div class="youtube" style="padding-top:${(100 / attrs.width) *
+        attrs.height}%"><iframe src="https://www.youtube.com/embed/${
+        attrs.youtube
+      }" frameborder="0"></iframe></div>`,
+  );
 
   shortcodes.add({
-    pow: (attrs) => (parseInt(attrs.pow, 10) ** 2),
-    sqrt: (attrs) => Math.sqrt(parseInt(attrs.sqrt, 10)),
+    pow: attrs => parseInt(attrs.pow, 10) ** 2,
+    sqrt: attrs => Math.sqrt(parseInt(attrs.sqrt, 10)),
   });
 
-  [
-    'works',
-    'parameters',
-    'ignore',
-  ].forEach((test) => {
-    const input = fs.readFileSync(path.resolve(__dirname, 'fixtures', `${test}.txt`), 'utf8');
+  ['works', 'parameters', 'ignore'].forEach(test => {
+    const input = fs.readFileSync(
+      path.resolve(__dirname, 'fixtures', `${test}.txt`),
+      'utf8',
+    );
 
     it(test, () => {
       expect(shortcodes.parse(input)).toMatchSnapshot();
