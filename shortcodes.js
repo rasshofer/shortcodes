5c9c19c80f1202dbc9b9e66891d6ba948728f51d
Thomas Rasshofer <hello@thomasrasshofer.com>
2020-01-25 16:52:43 +0100

    Switch from JSHint to ESLint

--- shortcodes.js
+++ shortcodes.js
@@ -10,30 +10,24 @@ const add = (generic, callback) => {
   }
 };
 
-const parse = (input) => {
+const parse = (input) => input.replace(/(?=[^\]])\(([a-z0-9_-]+):(.*?)\)/gmi, (match, name, values) => {
+  if (!shortcodes[name]) {
+    return match;
+  }
 
-  return input.replace(/(?=[^\]])\(([a-z0-9_-]+):(.*?)\)/gmi, (match, name, values) => {
+  const attrs = {};
+  const splitted = values.split(/(\s*([a-z0-9_-]+):\s*)/gmi);
 
-    if (!shortcodes[name]) {
-      return match;
-    }
+  attrs[name] = splitted.shift().trim();
 
-    const attrs = {};
-    const splitted = values.split(/(\s*([a-z0-9_-]+)\:\s*)/gmi);
+  for (let i = 0; i < splitted.length; i += 3) {
+    attrs[splitted[i + 1].trim()] = splitted[i + 2].trim();
+  }
 
-    attrs[name] = splitted.shift().trim();
-
-    for (let i = 0; i < splitted.length; i += 3) {
-      attrs[splitted[i + 1].trim()] =  splitted[i + 2].trim();
-    }
-
-    return shortcodes[name](attrs);
-
-  });
-
-};
+  return shortcodes[name](attrs);
+});
 
 module.exports = {
   add,
-  parse
+  parse,
 };
--- test/tests.js
+++ test/tests.js
@@ -11,24 +11,20 @@ tap.equal(typeof shortcodes.parse, 'function');
 
 // Parsing
 
-shortcodes.add('youtube', (attrs) => {
-  return `<div class="youtube" style="padding-top:${(100 / attrs.width * attrs.height)}%"><iframe src="https://www.youtube.com/embed/${attrs.youtube}" frameborder="0"></iframe></div>`;
-});
+shortcodes.add('youtube', (attrs) => `<div class="youtube" style="padding-top:${(100 / attrs.width * attrs.height)}%"><iframe src="https://www.youtube.com/embed/${attrs.youtube}" frameborder="0"></iframe></div>`);
 
 shortcodes.add({
-  pow: (attrs) => Math.pow(parseInt(attrs.pow, 10), 2),
-  sqrt: (attrs) => Math.sqrt(parseInt(attrs.sqrt, 10))
+  pow: (attrs) => (parseInt(attrs.pow, 10) ** 2),
+  sqrt: (attrs) => Math.sqrt(parseInt(attrs.sqrt, 10)),
 });
 
 [
   'works',
   'parameters',
-  'ignore'
+  'ignore',
 ].forEach((test) => {
-
   const input = fs.readFileSync(path.resolve(__dirname, test, 'input.txt'), 'utf8');
   const output = fs.readFileSync(path.resolve(__dirname, test, 'output.txt'), 'utf8');
 
   tap.equal(shortcodes.parse(input), output);
-
 });
